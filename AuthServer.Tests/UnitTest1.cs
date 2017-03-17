7e4d72b7e396b0b58244f02d1cf38fe39e45603e
Andrew Holm <Andrew.Holm@carvana.com>
2017-03-16 23:43:22 -0700

    first commit

---
+++ AuthServer.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AuthServer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
